48cb330e58442794ae7e7c92f8ff848a174162e9
I509180 <f.flores@sap.com>
2019-12-02 18:16:14 -0300

    initial main skeleton

---
+++ minesweeper_solver.py
@@ -0,0 +1,11 @@
+def main():
+    #mainloop:
+        # window to image
+        # image to matrix
+        # matrix to best play
+        # click on the game
+        # check if end game
+    print("lets sweep")
+
+if __name__ == "__main__":
+    main()
