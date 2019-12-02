03b1108b96ba56276496164f786862d85a94e6ac
I509180 <f.flores@sap.com>
2019-12-02 19:02:10 -0300

    adds flag mines

--- minesweeper_solver.py
+++ minesweeper_solver.py
@@ -2,8 +2,9 @@ def main():
     #mainloop:
         # window to image
         # image to matrix
-        # matrix to best play
-        # click on the game
+        # matrix to best play and mine
+        # click to flag mines
+        # click the play on the game
         # check if end game
     print("lets sweep")
 
