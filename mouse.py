930923fb319d5e3551ef895528c6dcb67d77e71d
FelipeMFlores <felipe.melloflores@gmail.com>
2019-12-03 22:13:15 -0300

    mouse clicks

---
+++ mouse.py
@@ -0,0 +1,31 @@
+import subprocess
+import time
+
+
+def _get_window_id():
+    proc = subprocess.run(["xdotool", "search", "--onlyvisible", "--classname",
+                           "mines"], check=True, stdout=subprocess.PIPE,
+                          universal_newlines=True)
+    return proc.stdout
+
+
+def _move_mouse(window, x, y):
+    subprocess.run(["xdotool", "mousemove", "--clearmodifiers", "--sync",
+                "--window", window, str(x), str(y)],
+                check=True)
+
+
+def click_mine(x, y):
+    output = _get_window_id()
+    _move_mouse(output, x, y)
+    time.sleep(0.1)
+    subprocess.run(["xdotool", "click", "1"],
+                   check=True)
+
+
+def click_flag(x, y):
+    output = _get_window_id()
+    _move_mouse(output, x, y)
+    time.sleep(0.1)
+    subprocess.run(["xdotool", "click", "3"],
+                   check=True)
